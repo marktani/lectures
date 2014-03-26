35ee539518fdfa6da3b9a9a34330d57a6ce366d7
Nilan <nilan.marktanner@student.kit.edu>
2014-03-26 16:39:55 +0100

    Finished Lesson 2 - Content Type

--- udacity/webdev/helloworld/helloworld.py
+++ udacity/webdev/helloworld/helloworld.py
@@ -2,12 +2,17 @@
 
 import webapp2
 
+form = """
+<form action="http://www.google.com/search">
+	<input name="q">
+	<input type="submit">
+</form>
+"""
 
 class MainPage(webapp2.RequestHandler):
-
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, Udacity!')
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(form)
 
 
 application = webapp2.WSGIApplication([
