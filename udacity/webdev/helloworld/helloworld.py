5848f4c11380e2eb0359385c23855b108e47e6af
Nilan <nilan.marktanner@student.kit.edu>
2014-03-26 16:17:41 +0100

    Finished lesson 1

---
+++ udacity/webdev/helloworld/helloworld.py
@@ -0,0 +1,16 @@
+"""Simple webapp2 server."""
+
+import webapp2
+
+
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, Udacity!')
+
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
+
